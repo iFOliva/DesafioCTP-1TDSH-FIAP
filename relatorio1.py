5c9394ffbb54739de85e2da5100d898040e2cd0d
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-17 07:50:53 -0300

    refactor: use util functions

--- relatorio1.py
+++ relatorio1.py
@@ -1,46 +1,10 @@
 import pandas as pd
-import unidecode
 import util.average as avg
+from util.getColumns import getColumns
+from util.getSubject import getSubject
+from util.getStudents import getStudents
 
-df = pd.read_excel('./samples/sample.xlsx')
-
-
-def getSubject():
-    header = df.columns
-    subject = header[2]
-
-    return subject
-
-
-def getColumns():
-    columns = []
-
-    for col in range(df.shape[1]):
-        value = str(df.iat[1, col])
-        if (value != 'nan'):
-            columns.append(unidecode.unidecode(value))
-
-    return columns
-
-
-def getStudents(columns):
-    students = []
-
-    for row in range(df.shape[0]):
-        if(row > 1):
-            i = 0
-            student = {}
-
-            for col in range(df.shape[1]):
-                value = str(df.iat[row, col])
-
-                if(value.lower() != 'nan'):
-                    student[columns[i]] = value
-                    i += 1
-
-            students.append(student)
-
-    return students
+df = pd.read_excel('./samples/notas_planilha_sample.xlsx')
 
 
 def printStudents(students, subject):
@@ -56,26 +20,27 @@ def printStudents(students, subject):
 
         MS1Value = float(student['Semestre_1'])
 
-        finalGCS = avg.calcFinalGS(
-            ms1=MS1Value, checkpointAverage=checkpointAverage, challengeAverage=challengeAverage)
+        finalGCS = abs(avg.calcFinalGS(
+            ms1=MS1Value, checkpointAverage=checkpointAverage, challengeAverage=challengeAverage))
 
         print("\n\n===================================================")
         print(" Disciplina: " + subject + "\n")
         print(" RM: %g" % float(student['RM']) + ", " + student['Nome'] + "\n")
 
-        print(" Semestre 1: " + str(MS1Value) + "\n")
+        print(" Semestre 1: " + str(round(MS1Value, 1)) + "\n")
 
         print(" \tSemestre 2:")
 
-        print(" Checkpoints (média): " + str(checkpointAverage))
-        print(" Challenge (média): " + str(challengeAverage) + "\n")
+        print(" Checkpoints (média): " + str(round(checkpointAverage, 1)))
+        print(" Challenge (média): " + str(round(challengeAverage, 1)) + "\n")
         print("---------------------------------------------------")
-        print(" Nota mínima na Global Solution para aprovação: " + str(finalGCS))
+        print(" Nota mínima na Global Solution para aprovação: " +
+              str(round(finalGCS, 1)))
         print("===================================================")
 
 
-subject = getSubject()
-columns = getColumns()
-students = getStudents(columns)
+subject = getSubject(df)
+columns = getColumns(df)
+students = getStudents(columns, df)
 
 printStudents(students, subject)
